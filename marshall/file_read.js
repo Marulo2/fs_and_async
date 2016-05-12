9e846d655302ab1ea3b16cd52cdccb4940dc7f1e
Mars <marshallmiller92@gmail.com>
2016-05-12 12:31:26 -0700

    What am I even doing

--- marshall/file_read.js
+++ marshall/file_read.js
@@ -2,39 +2,95 @@ const fs = require('fs');
 const EventEmitter = require('events').EventEmitter;
 const ee = new EventEmitter();
 
+// ee.on('file-read', (data) => {
+//   console.log('READ: ', data.toString());
+// })
+//
+// function makeReadFile(txt){
+//   return function(){
+//     fs.readFile(txt, (err, data) => {
+//       console.log(data);
+//       ee.emit('file-read', data);
+//     });
+//   };
+// };
+//
+// var one = makeReadFile('./one.txt');
+// var two = makeReadFile('./two.txt');
+// var three = makeReadFile('./three.txt');
+//
+// one();
+// two();
+// three();
+//
+// ///
+//
+// ee.on('read', function(){
+//   console.log('test');
+// });
+//
+// ee.emit('read');
+
+/// --------------------
+
+// var database = {
+//   text: [
+//     { path: './one.txt' },
+//     { path: './two.txt' },
+//     { path: './three.txt' }
+//   ]
+// };
+//
+// function readText(){
+//   EventEmitter.call(this);
+// }
+//
+// readText.prototype.save = function(obj){
+//   database.text.push(obj);
+//   this.emit('saved-text', obj);
+// };
+//
+// readText.prototype.all = function(){
+//   return database.text;
+// }
+//
+// module.exports = readText;
+
+///-------------------------
+
+// var fsp = require('fs-promise');
+//
+// new Promise(
+//   function(resolve){
+//
+//   }
+// )
+
+
+///---------------------------
+
+ee.on('event', () => {
+  process.nextTick(() => {
+    fs.readFile('./one.txt', (err, data) => {
+      console.log(data);
+      ee.emit('file-read', data);
+    });
+  });
+});
+
+ee.emit('event');
+
 ee.on('file-read', (data) => {
   console.log('READ: ', data.toString());
 })
 
-function makeReadFile(txt){
-  return function(){
-    fs.readFile(txt, (err, data) => {
-      console.log(data);
-      ee.emit('file-read', data);
-    });
-  };
-};
-
-var one = makeReadFile('./one.txt');
-var two = makeReadFile('./two.txt');
-var three = makeReadFile('./three.txt');
-
-one();
-two();
-three();
-
-///
-
-var fsp = require('fs-promise');
-
-new Promise(
-  function(resolve){
-
-  }
-)
-
-///
-
-function readFile(txt){
-  return fs.readFile(txt)
-}
+//
+// function makeReadFile(txt){
+//   return function(){
+//     fs.readFile(txt, (err, data) => {
+//       console.log(data);
+//       ee.emit('file-read', data);
+//     });
+//   };
+// };
+//
--- marshall/test/test_read.js
+++ marshall/test/test_read.js
@@ -1,13 +1,11 @@
 const chai = require('chai').expect;
 const read = require(__dirname + '../file_read');
+const one = require(__dirname + './one.txt')
+const two = require(__dirname + './two.txt')
+const three = require(__dirname + './three.txt')
 
 describe('first async test', () => {
   it('should read async code', (done) => {
-    readPost(__dirname + '../file_read.js')
-     .then(function(data){
-       data.should.equal('content read');
-       done();
-     })
-     .catch(done);
-  })
-})
+    if (err)
+  });
+});
